c70e879333e22e3baf56ef6d282136191a9ff245
isprzedamto <sprzedamto2025@gmail.com>
2025-07-07 18:52:16 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Layout from '@/components/Layout'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
@@ -30,4 +30,4 @@ export default function Home() {
       </p>
     </Layout>
   )
-    }
+}
