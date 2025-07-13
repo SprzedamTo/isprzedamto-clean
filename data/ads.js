20f0647b7502741403f62eb3653603aee523f093
isprzedamto <sprzedamto2025@gmail.com>
2025-07-14 00:27:21 +0200

    Create ads.js

---
+++ data/ads.js
@@ -0,0 +1,37 @@
+const ads = [
+  {
+    id: 1,
+    title: "Opel Astra 1.6 Benzyna",
+    description: "Zadbany Opel Astra z małym przebiegiem, serwisowany.",
+    price: "11 500 zł",
+    location: "Kraków",
+    image: "/car1.jpg",
+    year: 2009,
+    engine: "1.6 Benzyna",
+    mileage: "168 000 km"
+  },
+  {
+    id: 2,
+    title: "Kanapa rozkładana IKEA",
+    description: "Stan bardzo dobry. Używana przez rok.",
+    price: "350 zł",
+    location: "Warszawa",
+    image: "/kanapa.jpg",
+    year: 2021,
+    engine: "",
+    mileage: ""
+  },
+  {
+    id: 3,
+    title: "iPhone 12 Pro 128GB",
+    description: "W idealnym stanie, bez blokad. Kolor: Pacific Blue.",
+    price: "2 400 zł",
+    location: "Poznań",
+    image: "/iphone12.jpg",
+    year: 2022,
+    engine: "",
+    mileage: ""
+  }
+];
+
+export default ads;
